6cd6d8cc0a39f1f6a104b000902c669213b52261
Patrik Cesnek <pcesnek290@gmail.com>
2025-03-21 12:49:46 +0100

    Add Mock

---
+++ SpaceXLaunches/Constants&Mock/Mock.swift
@@ -0,0 +1,20 @@
+//
+//  Mock.swift
+//  SpaceXLaunches
+//
+//  Created by Patrik Cesnek on 21/03/2025.
+//
+
+struct Mock {
+    static let mockLaunch = Launch(
+        missionName: "Crew-1",
+        launchDate: "16-11-2020",
+        rocket: mockRocket,
+        details: mockDetails,
+        links: mockLinks
+    )
+    static let mockRocket = Rocket(rocketName: "Falcon 9")
+    static let mockLinks = Links(missionPatch: nil)
+    
+    static let mockDetails = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+}
